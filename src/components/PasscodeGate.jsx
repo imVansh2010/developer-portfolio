77404c9251b491540066927b92da256e07f64ba7
Vansh <agrwlvansh@gmail.com>
2026-09-19 10:07:12 -0700

    Update text color for the passcode gate prompt

--- src/components/PasscodeGate.jsx
+++ src/components/PasscodeGate.jsx
@@ -53,7 +53,7 @@ export default function PasscodeGate({ onUnlock }) {
           </div>
 
           <div className="p-8 xl:p-10">
-            <p className="font-mono text-[15px] text-com xl:text-base">
+            <p className="font-mono text-[15px] text-fg xl:text-base">
               <span className="mr-1.5 text-accent">$</span>./open-portfolio --preview
             </p>
 
